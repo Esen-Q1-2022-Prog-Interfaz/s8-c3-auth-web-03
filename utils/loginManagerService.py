fd79b15d8e0f74e8925203c21d2c34ea73ccbdf7
balbino aylagas <balbino_a@hotmail.com>
2022-03-12 10:41:23 -0600

    init files

---
+++ utils/loginManagerService.py
@@ -0,0 +1,13 @@
+from flask_login import LoginManager
+from models.user import User
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print(user_id)
+    user = User.query.filter_by(id=user_id).first()
+    print(user)
+    return user
